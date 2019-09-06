1df10be991962333817eecc5a273f39bd89173f7
unknown <dks4mii@gmail.com>
2019-09-06 14:55:03 +0500

    Removed Addiotional folder

--- server.js
+++ server.js
@@ -1,37 +1,25 @@
-const ZKLib = require('zklib');
- 
-ZK = new ZKLib({
-  ip: '110.37.224.158',
-  port: 4370,
-  inport: 5200,
-  timeout: 5000,
-});
- 
-// // connect to access control device
-ZK.connect(function(err) {
-  if (err) throw err;
- 
-  // read the time info from th device
-  ZK.getAttendance(function(err, t) {
-    // disconnect from the device
-    ZK.disconnect();
- 
-    if (err) throw err;
- 
-    console.log(t);
-  });
-});
-// connect to access control device
-// ZK.connect(function(err) {
-//     if (err) throw err;
-   
-//     // read the time info from th device
-//     ZK.getUser(function(err, t) {
-//       // disconnect from the device
-//       ZK.disconnect();
-   
-//       if (err) throw err;
-   
-//       console.log(t);
-//     });
-//   });
+var MongoClient = require('mongodb').MongoClient;
+const express = require('express')
+const bodyParser= require('body-parser')
+const Attendence = require('./Routes/Attendence/index')
+
+const app = express();
+var url = "mongodb://localhost:27017/mydb";
+
+
+app.use(bodyParser.urlencoded({extended: true,limit: '50mb'}))
+app.use(bodyParser.json({limit: '50mb'})); 
+app.set('view engine', 'ejs')
+
+app.use('/Attendence',Attendence)
+
+
+
+MongoClient.connect(url, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('test') 
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
